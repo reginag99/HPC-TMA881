88866b66a0ba710bd44753a8bcbfe8b420b541aa
Ida <ida.berg.home@gmail.com>
2023-10-06 18:02:54 +0200

    Förslag på distansberäkning

--- Idas.c
+++ Idas.c
@@ -1,109 +1,167 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <omp.h>
 #include <ctype.h>
-#include <math.h>
+#include <math.h>   
 
-//Plocka ut siffran i -t5 och                                                                                                                              
-//Måste lösningen kunna ta hänsyn till mellanslag?                                                                                                         
-//Om ja leta efter första siffran sedan atoi?                                                                                                              
+
+
+
+/*
+
+
+Har lagt in ett förslag på hur man kanske kan beräkna distanserna
+Får dock inte till allokeringarna när jag försöker lägga ihop alla distanser i distanceMatrix
+Har därför inte kunnat kolla om beräkningarna funkar...
+
+
+*/
+
+
 
 int compare (const void * a, const void * b);
 
-
-
 int main(int argc, char *argv[])
 {
-int numberRow = 10;
+int numberRow = 17;
 int numberCol = 3, numThreads;
 int argLenght = strlen(argv[1]);
 
-if (argv[1][0] == '-' && argv[1][1] == 't' && argLenght > 2)
-   {
+double distance, x1, x2, x3, y1, y2, y3;
+int block_row = 1000;
+int block_characters = 24;
+int block_size = block_row * block_characters;
+
+
+int size_distances_in_block = block_row*(block_row-1)/2;
+int sizeDistanceMatrix = numberRow*(numberRow-1)/2;
+int size_distances_between_blocks = sizeDistanceMatrix -size_distances_in_block;
+
+
+if (argv[1][0] == '-' && argv[1][1] == 't' && argLenght > 2) {
     numThreads = atoi(&argv[1][2]);
-   }
-else
-   {
+   } else  {
     printf("Wrong format for argument use format -t[INTEGER], where [INTEGER] must be valid whole number larger than 0.");
     exit(EXIT_FAILURE);
    }
 
-if (numThreads == 0)
-   {
+if (numThreads == 0){
     printf("INTEGER must be valid whole number larger than 0.");
     exit(EXIT_FAILURE);
    }
 
 printf("Number of threads %d\n", numThreads);
 
-float * matrixEntries = (float*) malloc(sizeof(float) * numberRow*numberCol);
-float ** matrix = (float**) malloc(sizeof(float*) * numberRow);
 
-for ( size_t ix = 0, jx = 0; ix < numberRow; ++ix, jx+=numberCol)
-   matrix[ix] = matrixEntries + jx;
 
-FILE *file = fopen("cells_10", "r");
+float * blockEntries = (float*) malloc(sizeof(float) * block_size);
+float **block = (float**) malloc(sizeof(float*) * block_row);
 
-if (file == NULL)
-   {
+double * distances_in_block =  (double*) malloc(sizeof(double) * size_distances_in_block);
+double * distances_between_blocks = (double*)malloc(sizeof(double) * size_distances_between_blocks);
+for ( size_t ix = 0, jx = 0; ix < block_row; ++ix, jx+=block_characters)
+   block[ix] = blockEntries + jx;
+
+
+
+
+FILE *file = fopen("test_data/cells_17", "r");
+if (file == NULL){
     printf("Error opening file.\n");
     return -1;
    }
 
-for (int ix = 0; ix < numberRow; ix++)
-     for (int jx = 0; jx < numberCol; jx++)
-     {
-      fscanf(file, "%f", &matrix[ix][jx]);
-     }
+
+
+for (int i = 0; i < numberRow; i += block_row) {
+    for (int ix = 0; ix < block_row; ix++)
+        fread(block[ix], sizeof(float), block_size, file);
+
+    for (int w = i + 1000; w < numberRow; w += 1000) {
+        for (int ix = 0; ix < block_row; ix++) {
+            int element = 0;
+            for (int j = 0; j < (block_row - 1); ++j) {
+               x1 = block[j][0];
+               x2 = block[j][1];
+               x3 = block[j][2];
+            for (int k = j + 1; k < block_row; ++k){
+               y1 = block[k][0];
+               y2 = block[k][1];
+               y3 = block[k][2];
+
+               distance = sqrt((x1-y1)*(x1-y1)+(x2-y2)*(x2-y2)+(x3-y3)*(x3-y3));
+               distances_in_block[element] = (int)(distance * 100 + 0.5) / 100.0;
+               element += 1;
+               
+               }
+            }
+         }
+        for (int ix1 = i; ix1 < i + block_row; ix1++) 
+        {   int element_between = 0;
+            for (int ix2 = w; ix2 < w + block_row; ix2++) 
+            {
+                x1 = block[ix1 - i][0];
+                x2 = block[ix1 - i][1];
+                x3 = block[ix1 - i][2];
+
+                y1 = block[ix2 - w][0];
+                y2 = block[ix2 - w][1];
+                y3 = block[ix2 - w][2];
+
+                distance = sqrt((x1 - y1) * (x1 - y1) + (x2 - y2) * (x2 - y2) + (x3 - y3) * (x3 - y3));
+                distances_between_blocks[element_between] = (int)(distance * 100 + 0.5) / 100.0;
+                element_between += 1;
+                
+            }
+        }
+    }
+}
+
+
+
+double *distanceMatrix = (double*) malloc(sizeof(double) * sizeDistanceMatrix);
+
+for (int i = 0; i < size_distances_in_block; i++) {
+    distanceMatrix[i] = distances_in_block[i];
+}
+
+for (int i = 0; i < size_distances_between_blocks; i++) {
+    distanceMatrix[size_distances_in_block + i] = distances_between_blocks[i];
+}
+
+
+
+for (int ix = 0 ; ix < sizeDistanceMatrix; ++ix)                                                                                                                                       
+    printf("%f\n",distanceMatrix[ix]);     
 
 fclose(file);
 
-//Räkna distanserna mellan koordinaterna                                                                                                                                                 
-
-int sizeDistanceMatrix = (numberRow*(numberRow-1)/2);
-double distance, x1, x2, y1, y2, z1, z2, temp;
-int element = 0;
-
-double * distanceMatrix =  (double*) malloc(sizeof(double) * sizeDistanceMatrix);
-
-for (int ix = 0 ; ix < (numberRow - 1) ; ++ix)
-   {
-        x1 = matrix[ix][0];
-        y1 = matrix[ix][1];
-        z1 = matrix[ix][2];
-    for (int jx = ix + 1; jx < numberRow; ++jx )
-        {
-        x2 = matrix[jx][0];
-        y2 = matrix[jx][1];
-        z2 = matrix[jx][2];
-
-        distance = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
-        distanceMatrix[element] = (int)(distance * 100 + 0.5) / 100.0;
-        element += 1;
-        }
-   }
+free(block);
+free(blockEntries);
+free(distances_in_block);
+free(distances_between_blocks);
 
 
 
-free(matrix);
-free(matrixEntries);
+
+
 
 for( int ix = 0; ix < sizeDistanceMatrix; ++ix)
     {
         for(int jx = ix + 1; jx < sizeDistanceMatrix; ++jx)
         { if(distanceMatrix[ix]> distanceMatrix[jx])
             {
-                temp = distanceMatrix[ix];
+                double temp = distanceMatrix[ix];
                 distanceMatrix[ix] = distanceMatrix[jx];
                 distanceMatrix[jx] = temp;
             }
         }
     }
 
-
-//for (int ix = 0 ; ix < sizeDistanceMatrix; ++ix)                                                                                                                                       
-//    printf("%f\n",distanceMatrix[ix]);                                                                                                                                                 
+// for (int ix = 0 ; ix < sizeDistanceMatrix; ++ix)                                                                                                                                       
+//     printf("%f\n",distanceMatrix[ix]);                                                                                                                                                 
 
 float largestElement = distanceMatrix[sizeDistanceMatrix-1];
 
@@ -111,8 +169,8 @@ float largestElement = distanceMatrix[sizeDistanceMatrix-1];
 
 int sizeFrequencyMatrix = largestElement * 100; //Antag att vi har två decimaler                                                                                                         
 
-float * frequencyMatrixEntries = (float*) malloc(sizeof(float) * sizeFrequencyMatrix*2);
-float ** frequencyMatrix = (float**) malloc(sizeof(float*) * 2);
+float *frequencyMatrixEntries = (float*) malloc(sizeof(float) * sizeFrequencyMatrix*2);
+float **frequencyMatrix = (float**) malloc(sizeof(float*) * 2);
 
 for ( size_t ix = 0, jx = 0; ix < 2; ++ix, jx+= sizeFrequencyMatrix) //Vilken ordning är bäst??                                                                                          
    frequencyMatrix[ix] = frequencyMatrixEntries + jx;
@@ -128,11 +186,14 @@ for (size_t ix = 0; ix < sizeDistanceMatrix; ++ix)//Vilken ordning är bäst??
    {
     int jx = (int)(distanceMatrix[ix]*100);
     frequencyMatrix[0][jx] = distanceMatrix[ix];
-    frequencyMatrix[1][jx] =+ 1;
+    frequencyMatrix[1][jx] += 1;
    }
+
+
 
 free(distanceMatrix);
 
+printf("Before printing distanceMatrix\n");
 for (int ix = 0; ix < sizeFrequencyMatrix; ++ix)
 {
    if (frequencyMatrix[0][ix] != 0.00)
@@ -140,6 +201,7 @@ for (int ix = 0; ix < sizeFrequencyMatrix; ++ix)
     printf("%.2f %.2f \n",frequencyMatrix[0][ix],frequencyMatrix[1][ix]);
    }
 }
+printf("After printing distanceMatrix\n");
 
 free(frequencyMatrix);
 free(frequencyMatrixEntries);
@@ -147,7 +209,3 @@ free(frequencyMatrixEntries);
  return 0;
 
 }
-
-
-
-
