df896f4f2e9c6f54e261839a46619cd55c3e7399
LisaVind <75325806+LisaVind@users.noreply.github.com>
2023-10-13 17:57:23 +0200

    Update sync_file.c

--- sync_file.c
+++ sync_file.c
@@ -29,7 +29,7 @@ typedef struct {
   float **wReal; // Vill vi använda denna för att utföra operationer? 
   float **wImg; // Vill vi använda denna för att utföra operationer?
   int ib; //increment ib with istep until size sz is reached, ska variera med trådar
-  int istep;
+  int stepSize;
   int lines; //Stoppa iterationen när vi har itererat igenom alla y värden!!
   int tx; 
   mtx_t *mtx; //Mutex, used to syncronize work need to be destoryed at the end of program
@@ -43,13 +43,60 @@ typedef struct {
   const float **v; //Double pointer "collection of lines" "pixel"??
   float **w;
   int ib; //increment ib with istep until size sz is reached
-  int istep;
+  int stepSize;
   int sz; //Vill vi ha en annan storlek?
   int tx; 
   mtx_t *mtx; //Mutex, used to syncronize work need to be destoryed at the end of program
   cnd_t *cnd; // conditional variable, used to syncronize work need to be destoryed at the end of program
   int_padded *status;// padded used for treating false sharing, used to communicate status/progress of threads in processing its lines
 } thrd_info_t;
+
+//This should do the actual computation!!
+int
+main_thrd(
+    void *args
+    )
+{ 
+  //Copying things to local variables
+  const thrd_info_t *thrd_info = (thrd_info_t*) args;
+  const float **vReal = thrd_info->vReal; //Kan hårdkodas
+  const float **vImg = thrd_info->vImg; //Kan hårdkodas
+  float **wRel = thrd_info->wRel;
+  float **wImg = thrd_info->wImg;
+  const int ib = thrd_info->ib;//Vad är ib skickar vi in det?
+  const int stepSize = thrd_info->stepSize;
+  const int lines = thrd_info->lines;
+  const int tx = thrd_info->tx;
+  mtx_t *mtx = thrd_info->mtx;
+  cnd_t *cnd = thrd_info->cnd;
+  int_padded *status = thrd_info->status;
+
+  //const float *vix_Img = vImg[]; //Hämta ut en konstant float
+
+  for ( int ix = ib; ix < sz; ix += stepSize ) {
+    const float *vix_real = vReal[ix]; 
+    // We allocate the rows of the result before computing, and free them in another thread.
+    float *wix_real = (float*) malloc(sz*sizeof(float)); //Beräknar vi en rad i taget eller 1 element?
+    float *wix_img = (float*) malloc(sz*sizeof(float)); //
+
+    for ( int jx = 0; jx < sz; ++jx )
+      wix_real[jx] = vix_real[jx];
+      //wix_img[jx] = vix[jx];
+
+    //Check thread to see if further progress has been made?
+    mtx_lock(mtx);
+    wix_real[jx] = wix_real;
+    status[tx].val = ix + stepSize;
+    mtx_unlock(mtx);
+    cnd_signal(cnd);
+
+    // In order to illustrate thrd_sleep and to force more synchronization
+    // points, we sleep after each line for one micro seconds.
+    thrd_sleep(&(struct timespec){.tv_sec=0, .tv_nsec=1000}, NULL);
+  }
+
+  return 0;
+}
 
 
 int
@@ -85,8 +132,15 @@ main()
 
   //Allt i main thread funktionen kommer göras av alla trådar! 
 
-  float **wReal = (float**) malloc(lines*sizeof(float*));
-  float **wImg = (float**) malloc(lines*sizeof(float*));
+  //Fel dessa vill vi ska vara det vi sparar under newton iterationerna
+  // ska innehålla 1 real del + imaginär del + antalet iterationer och vilken rot vi konvergerar till??
+  float **colors = (float**) malloc(lines*sizeof(float*));
+  float **nIterates = (float**) malloc(lines*sizeof(float*));
+  
+  
+  //Vill vi spara koordinaten med??
+  //float **wReal = (float**) malloc(lines*sizeof(float*));
+  //float **wImg = (float**) malloc(lines*sizeof(float*));
   // The entries of w will be allocated in the computation threads are freed in
   // the check thread.
 
@@ -109,14 +163,19 @@ main()
   int_padded status[numThreads]; //Each thread needs its own status variable
 
 //Martin gör for loop för att skapa numThreads antal trådar och kalla på main_thrd function iterera mellan antalet trådar, 
-//skicka in vReal, vImg  vad mer??
+//skicka in vReal, vImg, trueRoots  vad mer??
    for ( int tx = 0; tx < numThreads; ++tx ) {
-    int r = thrd_create(thrds+tx, main_thrd, (void*) (thrds_info+tx));
+    //Initializes local variables for threads
+
+    //Kalla på main_thrd funktionen!
+    int r = thrd_create(thrds+tx, main_thrd, (void*) (thrds_info+tx));//thrds_info+tx <- start punkten, vad är ib?
     if ( r != thrd_success ) {
       fprintf(stderr, "failed to create thread\n");
       exit(1);
     }
    }
+
+   return 0;
 
 }
 
